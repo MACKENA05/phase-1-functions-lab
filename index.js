d299deaa74884a21990d157b28d6a380fc9e21cb
MonicahMackena¨ <mackenamuthui@gmail.com>
2024-11-27 21:59:12 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+ const hqBlock = 42 ;
+ return Math.abs (blockNumber - hqBlock);
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    const hqBlock = 42 ;
+    return Math.abs ((blockNumber - hqBlock)* 264)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math .abs ((destination - start)*264)
+}
+
+function calculatesFarePrice(start, destination) {
+const dist = Math .abs(destination - start)*264;
+    if (dist <= 400){
+        return 0
+    } else if (dist>400 && dist<=2000){
+        return (dist-400) * 0.02
+    }else if (dist>200 && dist<=2500) {
+        return  25
+    }else {
+        return 'cannot travel that far'
+    }
+}
+
